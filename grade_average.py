36190ac53898843dcf093dba953a3dd53cb93d94
joylourishcasa <joylourishcasa@gmail.com>
2021-09-21 09:42:42 +0000

    This is the third part of the program.

--- grade_average.py
+++ grade_average.py
@@ -6,7 +6,17 @@ final = float(input("Finals: "))
 avg = (prelim + midterm + semis + final)/4
 print("Average: {}" .format(avg))
 
-if (avg>=75):
-    print ("PASSED")
+if avg>=99 and avg<=100:
+    print("Your Average is {}" .format(avg) + "! Congratulations, you got 'A'!")
+elif avg>=90 and avg<=98:
+    print("Your Average is {}" .format(avg) + "! Good Job, you got 'B'!")
+elif avg>=80 and avg<=89:
+    print("Your Average is {}" .format(avg) + "! Nice work, you got 'C'!")
+elif avg>=71 and avg<=79:
+    print("Your Average is {}" .format(avg) + "! Averagely good, you got 'D'!")
+elif avg>=61 and avg<=70:
+    print("Your Average is {}" .format(avg) + "! Try Harder, you got 'E'!")
+elif avg<60:
+    print("Your Average is {}" .format(avg) + "! You failed, you got 'F'!")
 else:
-    print ("FAILED")
+    print("INVALID")
