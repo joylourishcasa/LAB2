e8c433e36db9e4a815a020ee34d64f5f91ffb806
joylourishcasa <joylourishcasa@gmail.com>
2021-09-21 09:38:06 +0000

    This is the second part of the program.

--- grade_average.py
+++ grade_average.py
@@ -5,3 +5,8 @@ semis = float(input("Semifinals: "))
 final = float(input("Finals: "))
 avg = (prelim + midterm + semis + final)/4
 print("Average: {}" .format(avg))
+
+if (avg>=75):
+    print ("PASSED")
+else:
+    print ("FAILED")
